29ab4431a687068788d8ac8d67002661e5356279
MarcoAntonioTR <u22208286@utp.edu.pe>
2024-09-03 09:11:08 -0500

    Creacion de views

---
+++ frontend/src/views/Home/Cat3/Procesadores.jsx
@@ -0,0 +1,18 @@
+import Header from "../../../components/layout/Header";
+import Footer from "../../../components/layout/Footer";
+import useTheme from "../../../components/hooks/useTheme";
+
+function Procesadores() {
+  const { isDarkTheme, toggleTheme } = useTheme();
+
+  return (
+    <div className={isDarkTheme ? "bg-dark text-light" : "bg-light text-dark"}>
+      <Header isDarkTheme={isDarkTheme} toggleTheme={toggleTheme} />
+      {/* Aquí iría el contenido principal de tu aplicación */}
+
+      <Footer isDarkTheme={isDarkTheme} />
+    </div>
+  );
+}
+
+export default Procesadores;
---
+++ frontend/src/views/Home/Componentes.jsx
@@ -0,0 +1,18 @@
+import Footer from "../../components/layout/Footer";
+import Header from "../../components/layout/Header";
+import useTheme from "../../components/hooks/useTheme";
+
+function Componentes() {
+  const { isDarkTheme, toggleTheme } = useTheme();
+
+  return (
+    <div className={isDarkTheme ? "bg-dark text-light" : "bg-light text-dark"}>
+      <Header isDarkTheme={isDarkTheme} toggleTheme={toggleTheme} />
+      {/* Aquí iría el contenido principal de tu aplicación */}
+
+      <Footer isDarkTheme={isDarkTheme} />
+    </div>
+  );
+}
+
+export default Componentes;
